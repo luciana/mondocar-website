67d79d7ceb59be47a9d3a370e6e7aea8dcf8f87a
luismed <luis_medeiros96@yahoo.com.br>
2020-12-11 20:09:52 -0300

    🔨 minor component adjustments on header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,62 +1,57 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import Logo from '../assets/mondologo.jpg';
 import { Link } from 'react-router-dom';
-import { Card } from 'react-bootstrap';
+import { Button, Card } from 'react-bootstrap';
 import $ from 'jquery';
 
 // import { Container } from './styles';
 
 function Header() {
-    const[ShowMenu, setShowMenu] = useState(-1);
+    const [ShowMenu, setShowMenu] = useState(-1);
 
     const handleShowToggle = (div) => {
-        if(ShowMenu === -1){
-            $('.Hamb').css('background-color',"rgb(182, 179, 179)");
-        }else{
-            $('.Hamb').css('background-color',"");
+        if (ShowMenu === -1) {
+            $('.Hamb').css('background-color', "rgb(182, 179, 179)");
+        } else {
+            $('.Hamb').css('background-color', "");
         }
-        console.log($('.Hamb').css('background-color'))
         setShowMenu(-ShowMenu);
     }
-  return(
-      <>
-      <div className="headerComp">
-          <div className="logo">
-            <img src={Logo} alt="logo"/>
-          </div>
-          <div className="navbar">
-            <Link to="/">
-                <span>Home</span>
-            </Link>
-            <Link to='/AboutUs'>
-                <span>About Us</span>
-            </Link>
-          </div>
-          <div className="collapseAll">
-          <div className="Hamb" onTouchStart={handleShowToggle} style={{cursor: 'pointer'}}>
-                <i className="fas fa-bars fa-2x"/>
-            </div>
+    return (
+        <>
+            <div className="headerComp">
+                <div className="logo">
+                    <img src={Logo} alt="logo" />
+                </div>
+                <div className="navbar">
+                    <Link to="/">
+                        <span>Home</span>
+                    </Link>
+                    <Link to='/AboutUs'>
+                        <span>About Us</span>
+                    </Link>
+                </div>
+                <div className="collapseAll">
+                    <div className="Hamb" onTouchStart={handleShowToggle} style={{ cursor: 'pointer' }}>
+                        <i className="fas fa-bars fa-2x" />
+                    </div>
 
-          </div>
-      </div>
-      <div className="collapseAll">
-          {ShowMenu === 1 && (
-                <Card>
-                    <Card.Body>
-                        <div className="collapseMenu">
-                            <Link to="/">
-                                <span>Home</span>
-                            </Link>
-                            <Link to='/AboutUs'>
-                                <span>About Us</span>
-                            </Link>
-                        </div> 
-                    </Card.Body>  
-                </Card>   
+                </div>
+            </div>
+            <div className="collapseAll">
+                {ShowMenu === 1 && (
+                    <div className="collapseMenu">
+                        <Link to="/">
+                            <span variant="secondary">Home</span>
+                        </Link>
+                        <Link to='/AboutUs'>
+                            <span variant="secondary">About Us</span>
+                        </Link>
+                    </div>
                 )}
-      </div>
-      </>
-  );
+            </div>
+        </>
+    );
 }
 
 export default Header;
