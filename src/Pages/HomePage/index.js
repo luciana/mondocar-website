000dd254d67414bff69a6014e305ff302aca19a3
luismed <luis_medeiros96@yahoo.com.br>
2020-12-12 12:26:36 -0300

    🔨 map and location integrated

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import { Carousel } from 'react-bootstrap';
 import { Header } from '../../Components/index';
-import CarouselImg from '../../assets/images/cars/IMG_2062.JPG';
-// import { Container } from './styles';
+import { useGoogleMaps } from "react-hook-google-maps";
+
+
 
 export default function HomePage() {
+  const { ref, map, google } = useGoogleMaps(
+    "AIzaSyC8ytbqfN7jHZE-yok4yfIaNUxE1qGJ2Mc",
+    {
+      center: { lat: 41.309825, lng: -81.431072 },
+      zoom: 16,
+    },
+  );
+  if (map) {
+    new google.maps.Marker({ position: { lat: 41.309825, lng: -81.431072 }, map });
+  }
+
   return (
     <div>
-      <div>
+      <div className="header">
         <Header></Header>
       </div>
-      <div className="carousell">
-        {/* <Carousel>
-          <Carousel.Item >
-            <div className="imageBlock d-block w-100">
-            </div>
-            <Carousel.Caption>
-                <h1>Mondo Car</h1>
-                <p>At Mondo Car we are experts in foreign and domestic cars.</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel> */}
+      <div className="Row1">
         <div id="carouselExampleSlidesOnly" className="carousel slide" data-ride="carousel">
           <div className="carousel-inner">
             <div className="carousel-item active">
@@ -31,6 +32,23 @@ export default function HomePage() {
               </div>
             </div>
           </div>
+        </div>
+      </div>
+      <div className="Row2">
+        <div className="title">
+          <span color="white">Who we are?</span>
+        </div>
+      </div>
+      <div className="Row3">
+        <div className="title">
+          <span color="white">How a get there?</span>
+        </div>
+        <div className="mapAdress">
+          <div ref={ref} className="map">
+          </div>
+          <a target="_blank" href="http://www.google.com/maps?f=q&hl=en&q=9713+Ravenna+Rd,+Twinsburg+Ohio+44087+USA&sll=37.0625,-95.677068&sspn=33.29802,59.238281&ie=UTF8&ll=41.309825,-81.431072&spn=0.007704,0.014462&z=16&iwloc=addr&om=1">
+            9713 Ravenna Rd. Twinsburg Ohio 44087
+            </a>
         </div>
       </div>
     </div>
